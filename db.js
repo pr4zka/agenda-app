4033573d961f28e3738fffabc7115c50ebcac249
ivan <ivanhakate1028@gmail.com>
2022-11-06 12:59:51 -0300

    db

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose')
-const { PASSWORD, DB, HOST, USER, DB_PORT } = require('./config/index')
+const mongoose = require("mongoose");
+const { PASSWORD, DB, HOST, USER, DB_PORT } = require("./config/index");
 
 const connection = async () => {
-    const db = await mongoose.connect(
-        `mongodb://${USER}:${PASSWORD}@${HOST}/${DB}:${DB_PORT}`
-    )
-    console.log('Connected to db', db.connection.host)
-}
+  const db = await mongoose.connect(
+    `mongodb://${{ USER }}:${{ PASSWORD }}@${{ HOST }}:${{ DB_PORT }}`
+  );
+  console.log("Connected to db", db.connection.host);
+};
 
-module.exports = {connection, mongoose}
+module.exports = { connection, mongoose };
